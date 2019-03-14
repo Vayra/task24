53f6b5868cbfbd0e416402d0eb3d9043d6462744
Kenneth Ngo <kenneth.ngo@no.experis.com>
2019-03-14 09:33:01 +0100

    Imported handlebars

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
-const app = express()
-const port = 8080
+const express = require("express");
+const handlebars = require("handlebars");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+const port = 8080;
 
-let rawData = fs.readFileSync('data.json')
-const data = JSON.parse(rawData)
+let rawData = fs.readFileSync("data.json");
+const data = JSON.parse(rawData);
 
-app.get('/data', (req, res) => res.send(data))
+app.get("/data", (req, res) => res.send(data));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 8080);
