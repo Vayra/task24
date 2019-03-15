08bffc6fcc934ad758cd84ac960ac210c21b80ea
Kenneth Ngo <kenneth.ngo@no.experis.com>
2019-03-15 11:31:48 +0100

    My work

--- app.js
+++ app.js
@@ -4,74 +4,78 @@ const fs = require("fs");
 const path = require("path");
 const app = express();
 const port = 8080;
-var jsdom = require('jsdom');
+var jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const { window } = new JSDOM();
-const { document } = (new JSDOM('')).window;
+const { document } = new JSDOM("").window;
 global.document = document;
 
-var $ = jQuery = require('jquery')(window);
+var $ = (jQuery = require("jquery")(window));
 
 let rawData = fs.readFileSync("data.json");
 const data = JSON.parse(rawData);
 
-app.use('/html', express.static('html'));
+app.use("/html", express.static("html"));
+app.use("/css", express.static("css"));
 // Handlebars stuff
-
 
 //Contexts:
 let experienceContext = {
-    workExperience : data.workExperience
+  workExperience: data.workExperience
 };
 let contactContext = {
-    name: data.name,
-    title: data.title,
-    profilePicture: data.profilePicture
+  name: data.name,
+  title: data.title,
+  profilePicture: data.profilePicture
 };
 
 let educationContext = {
-    education: data.qualifications
-}
+  education: data.qualifications
+};
 
 let progressContext = {
-    code: data.codeInfo
-}
+  code: data.codeInfo
+};
 
 let linksContext = {
-    links: data.relevantLinks
-}
+  links: data.relevantLinks
+};
 
-let contexts = [experienceContext, contactContext, educationContext, progressContext, linksContext];
+let contexts = [
+  experienceContext,
+  contactContext,
+  educationContext,
+  progressContext,
+  linksContext
+];
 
 // Filenames
 let inFiles = [
-    'templates/experience-template.hbs',
-    'templates/contactInfo-template.hbs',
-    'templates/education-template.hbs',
-    'templates/progress-template.hbs',
-    'templates/links-template.hbs'
-]
+  "templates/experience-template.hbs",
+  "templates/contactInfo-template.hbs",
+  "templates/education-template.hbs",
+  "templates/progress-template.hbs",
+  "templates/links-template.hbs"
+];
 let outFiles = [
-    'html/experience-template.html',
-    'html/contactInfo-template.html',
-    'html/education-template.html',
-    'html/progress-template.html',
-    'html/links-template.html'
-]
+  "html/experience-template.html",
+  "html/contactInfo-template.html",
+  "html/education-template.html",
+  "html/progress-template.html",
+  "html/links-template.html"
+];
 
 // Loop through the templates and create html
-for (let i = 0; i < inFiles.length; i++){
-    let inFile = inFiles[i];
-    let outFile = outFiles[i];
-    let context = contexts[i];
-    let source = fs.readFileSync(inFile, 'utf8');
-    let template = handlebars.compile(source);
-    let compiled = template(context);
-    fs.writeFileSync(outFile, compiled);
-    //console.log(compiled);
+for (let i = 0; i < inFiles.length; i++) {
+  let inFile = inFiles[i];
+  let outFile = outFiles[i];
+  let context = contexts[i];
+  let source = fs.readFileSync(inFile, "utf8");
+  let template = handlebars.compile(source);
+  let compiled = template(context);
+  fs.writeFileSync(outFile, compiled);
+  //console.log(compiled);
 }
-
-
 
 app.get("/data", (req, res) => res.send(data));
 
